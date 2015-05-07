c57aada35906265424951ab764eee19803007fe7
Daniel Atkinson <hourd.tasa@gmail.com>
2015-05-07 21:37:59 +0100

    added default host information into database

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
+const os = require('os');
 const config = require('config');
+const PouchDB = require('pouchdb');
+var db;
+var environment;
 
-var configuration = config.get('Server');
+const configuration = config.get('Server');
 
-if (configuration.has('dbConfig')) {
-    if (configuration.dbConfig.type == 'pouchdb') {
-        const PouchDB = require('pouchdb');
-        var pouch = new PouchDB(configuration.dbConfig.location);
-        pouch.info().then(console.log.bind(console));
-    }
+function initialiseDatabase() {
+  if (configuration.has('dbConfig')) {
+      if (configuration.dbConfig.type == 'pouchdb') {
+          db = new PouchDB(configuration.dbConfig.location);
+          db.info().then(console.log.bind(console));
+      }
+  }
+    
+    db.get('environment', function(err, doc) {
+      if (err) {
+          var data = {
+              'host': os.hostname(),
+              'arch': os.arch(),
+              'type': os.type(),
+              'platform': os.platform(),
+              'release': os.release()
+          }
+          db.put(data, 'environment', function(err, response) {
+            if (err) {
+                console.log('put environment error');
+                return console.log(err);
+            }
+            return;
+          });
+          return;
+      }
+      console.log(doc);
+      return doc;
+    });
 }
 
+initialiseDatabase();
